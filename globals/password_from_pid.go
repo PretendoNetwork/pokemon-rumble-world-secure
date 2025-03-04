5f3bf2955a7306e742f58db8d67e14edb0c40eb7
SuperMarioDaBom <ajguerra2001@gmail.com>
2025-03-04 13:55:23 -0800

    Remove tester requirement check
    Prod-level users should now be able to connect

--- globals/password_from_pid.go
+++ globals/password_from_pid.go
@@ -19,11 +19,5 @@ func PasswordFromPID(pid types.PID) (string, uint32) {
 		return "", nex.ResultCodes.RendezVous.InvalidUsername
 	}
 
-	// * We only allow tester accounts for now
-	if response.AccessLevel < 1 {
-		globals.Logger.Errorf("PID %d is not a tester!", response.Pid)
-		return "", nex.ResultCodes.RendezVous.AccountDisabled
-	}
-
 	return response.Password, 0
 }
